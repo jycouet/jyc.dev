e874df13ee8d31da5a3e3a6dd143450e358a767e
jycouet <jycouet@gmail.com>
2024-03-15 12:54:56 +0100

    okay

--- src/routes/[date_and_slug]/+page.server.ts
+++ src/routes/[date_and_slug]/+page.server.ts
@@ -2,7 +2,10 @@ import { getMdInfo, getMdsInfo } from "$lib/mdInfos";
 import type { EntryGenerator } from "./$types";
 
 export const entries: EntryGenerator = () => {
-  return getMdsInfo();
+  const data = getMdsInfo();
+  console.log(`data`, data);
+
+  return data;
 };
 
 export const load = async ({ params }) => {
