b6a3e11519d81bae741c13ea26e23344d1c8e703
jycouet <jycouet@gmail.com>
2024-11-30 21:11:26 +0100

    rmv auto redirect

--- src/routes/stats/+layout.server.ts
+++ src/routes/stats/+layout.server.ts
@@ -5,9 +5,9 @@ import { remult } from 'remult'
 import type { LayoutServerLoad } from './$types'
 
 export const load = (async ({ url }) => {
-  if (url.host === 'jyc.dev') {
-    return redirect(308, url.href.replace('jyc.dev', 'skyzoo.blue'))
-  }
+  // if (url.host === 'jyc.dev') {
+  //   return redirect(308, url.href.replace('jyc.dev', 'skyzoo.blue'))
+  // }
 
   return { user: remult.user }
 }) satisfies LayoutServerLoad
